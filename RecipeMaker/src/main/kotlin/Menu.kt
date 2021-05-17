d3190a9d07b1d41a9886be58e83de1c1abed040d
PaladinMaple <karina@quadrosoluciones.mx>
2021-05-16 19:37:00 -0700

    Reto 2

--- RecipeMaker/src/main/kotlin/Menu.kt
+++ RecipeMaker/src/main/kotlin/Menu.kt
@@ -2,9 +2,9 @@ fun main(args: Array<String>) {
     println(".: Bienvenido a Recipe Maker :.\n");
 
     val menu:String = """Selecciona la opción deseada
-1. Hacer una receta
-2. Ver mis recetas
-3. Salir""".trimIndent();
+    1. Hacer una receta
+    2. Ver mis recetas
+    3. Salir""".trimIndent();
 
     var response:String? = null;
 
@@ -15,7 +15,34 @@ fun main(args: Array<String>) {
 
         if (response!!.toIntOrNull() == null)
             println("Solo introducir numeros");
-        else if (response!!.toIntOrNull()!! < 1 || response!!.toIntOrNull()!! > 3)
+        else if (response.toIntOrNull()!! < 1 || response.toIntOrNull()!! > 3)
             println("Opcion no valida");
+        else{
+            print(viewRecipe(response.toInt()))
+            if(response == "1")
+                makeRecipe();
+        }
+    }
+}
+
+fun makeRecipe()
+{
+    println("""Selecciona por categoría el ingrediente que buscas
+    1. Agua
+    2. Leche
+    3. Carne
+    4. Verduras
+    5. Frutas
+    6. Cereal
+    7. Huevos
+    8. Aceites""".trimIndent());
+}
+
+fun viewRecipe(selectedOption: Int): String {
+    return when(selectedOption){
+        1 -> "Hacer una receta\n";
+        2 -> "Ver mis recetas\n";
+        3 -> "Salir\n";
+        else -> "";
     }
 }
